79de0cd74cbb45246e3826b63fccf1c25a546421
Sudheer-18 <22p31a0424@acet.ac.in>
2025-12-14 14:09:58 +0530

    fina lchange1

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -20,7 +20,7 @@ function Footer() {
         <div>
           <img
             src="../src/assets/image.png"
-            alt="../src/assets/image.png"
+            alt="./assets/image.png"
             className="w-60 mb-8"
           />
 
